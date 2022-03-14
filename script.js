e440e26d5eefa900bd8c55cef222318d912408ed
oniprompt@gmail.com <=>
2022-03-13 23:50:26 -0300

    :up: consertadoa parte de tipografia e diminuido o espaco do css

--- script.js
+++
@@ -1,9 +0,0 @@
-const element = document.querySelector("#works-content");
-
-element.addEventListener("wheel", (event) => {
-  event.preventDefault();
-
-  element.scrollBy({
-    left: event.deltaY < 0 ? -30 : 30,
-  });
-});
